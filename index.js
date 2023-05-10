6e863b6bdcb569b10d495f75e3e10b689c1e4ec9
Hilton Vusi Kholokholo <68604335+Damox101za@users.noreply.github.com>
2023-05-10 12:42:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+'use strict';
+const http = require('http');
+const port = process.env.PORT || 1337;
 
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write(`Request path: ${req.url}\n`);
+    res.end('Hello World\n');
+}).listen(port);
